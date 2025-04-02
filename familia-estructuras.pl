1145a0be91c0b67a32fa69921d94df3bd970b827
Sarasvati Dallos <saricata1234567890@gmail.com>
2025-04-01 21:58:18 -0500

    Update familia-estructuras.pl

--- familia-estructuras.pl
+++ familia-estructuras.pl
@@ -4,3 +4,18 @@ persona(nombre, genero).
 % Definición de género
 hombres([abraham, homero, herbert, bart, clancy]).
 mujeres([mona, marge, patty, selma, lisa, maggie, jacqueline, ling]).
+
+% Definición de relaciones familiares
+padres([
+    (abraham, [homero, herbert]),
+    (clancy, [marge, patty, selma]),
+    (homero, [bart, lisa, maggie])
+]).
+
+madres([
+    (mona, [homero]),
+    (marge, [bart, lisa, maggie]),
+    (jacqueline, [marge, patty, selma]),
+    (selma, [ling])
+]).
+
