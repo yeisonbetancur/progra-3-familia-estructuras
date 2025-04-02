5f8cd6041f7128f40eb9822eeb7ce87239b188d2
ann123-max <ana.cabrales@utp.edu.co>
2025-04-01 23:03:35 -0500

    Update familia-estructuras.pl
    Se añadio una regla para saber si alguien es padre o madre de otra persona. Se añadio reglas de los abuelos

--- familia-estructuras.pl
+++ familia-estructuras.pl
@@ -19,3 +19,21 @@ madres([
     (selma, [ling])
 ]).
 
+%Definicion de una regla para acceder a los padres y sus hijos
+
+padre_de(Padre, Hijo) :-
+    padres(ListaPadres),
+    member((Padre, Hijos), ListaPadres),
+    member(Hijo, Hijos).
+    
+%Definicion de una regla para acceder a los padres y sus hijos
+
+madre_de(Madre, Hijo) :-
+    madres(ListaMadres),
+    member((Madre, Hijos), ListaMadres),
+    member(Hijo, Hijos).
+
+%Definicion de regla para los abuelos 
+abuelo_de(X,Y) :- padre_de(X,C), (padre_de(C,Y); madre_de(C,Y)), !.
+abuela_de(X,Y) :- madre_de(X,C), (madre_de(C,Y); padre_de(C, Y)), !.
+
