43f74f517ba6583b43cf77a3e72189e87e4f7030
Sarasvati Dallos <saricata1234567890@gmail.com>
2025-04-01 21:57:01 -0500

    Update familia-estructuras.pl

--- familia-estructuras.pl
+++ familia-estructuras.pl
@@ -1,1 +1,6 @@
+% Definición de estructura para personas
+persona(nombre, genero).
 
+% Definición de género
+hombres([abraham, homero, herbert, bart, clancy]).
+mujeres([mona, marge, patty, selma, lisa, maggie, jacqueline, ling]).
